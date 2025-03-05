f15868f4b55b994f07c2e64ef906c35da9d3ad82
lorenzobrendolin <149669070+lorenzobrendolin@users.noreply.github.com>
2025-03-05 11:16:36 +0100

    passo 24

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -131,3 +131,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- primo_progetto/urls.py
+++ primo_progetto/urls.py
@@ -28,6 +28,6 @@ urlpatterns = [
 ]
 
 #Add Django site authentication urls (for login, logout, password management)
-urlpatterns+=[
+urlpatterns += [
     path('accounts/', include('django.contrib.auth.urls')),
 ]
